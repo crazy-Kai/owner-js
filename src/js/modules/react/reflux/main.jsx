23cbd41367bd79a702790c51ba09f30e2963799d
apple <apple@appledeMacBook-Pro.local>
2016-07-08 00:31:07 +0800

    add

--- src/js/modules/react/reflux/main.jsx
+++ src/js/modules/react/reflux/main.jsx
@@ -2,53 +2,18 @@ define(function(require,exports,module){
   //依赖
   var React = require('react'),
       ReactDOM = require('reactDOM'),
-      Reflux = require('reflux');
-      console.log( React)
-   //创建Actions   
-  var TableActions = Reflux.createActions(['deleteName','addName','editName']);
-  //创建Store
-  var TableStore = Reflux.createStore({
-        listenables:[TableActions],
-        getInitialState:function(){
-            return this.store;
-        },
-        store : {
-            data:[{name:"wuxiaowen"},{name:"wukai"},{name:"一丙"},{name:"保健"}],
-            key:""
-        },
-        onDeleteName:function(event){
-            var self = this,
-                //react版本问题，这里不能用e.target否则找不到，只能用event.persist()方法来获取e.target
-                target = $(event.target),
-                index = target.data("index");
-                self.store.data.splice(index-1,1)
-                self.trigger(self.store);
-        },
-        onEditName:function(event,myInput){
-            var self = this,
-                index = $(event.target).data("index"),
-                key = index -1,
-                value = self.store.data[key].name,
-                
-                 input = myInput;
-                 input.value = value;
-                
-             
-                self.store.key = key;
-             
-                input.nextSibling.textContent = "保存";
-                input.focus();
-                self.trigger(self.store);
-        },
-        
-  })
-  
+      Controller = require('./controller');
+     
+   
    
   var TableBuild = React.createClass({
-        mixins : [Reflux.connect(TableStore)],
+        mixins : [Controller.Reflux.connect(Controller.Store)],
         handlefunction:function(e){
 
-            TableActions.editName(e,ReactDOM.findDOMNode(this.refs.myInput))
+            Controller.Actions.editName(e,ReactDOM.findDOMNode(this.refs.myInput))
+        },
+        createName:function(e){
+            Controller.Actions.addName(e,ReactDOM.findDOMNode(this.refs.myInput))
         },
         render:function(){
             //这里可以设置变量
@@ -87,7 +52,7 @@ define(function(require,exports,module){
                                                     </td>
                                                     <td>
                                                         <button className="fn-btn fn-btn-primary fn-MR10" data-index = {i} onClick = {self.handlefunction} >编辑</button>
-                                                        <button className="fn-btn fn-btn-primary" data-index = {i} onClick = {TableActions.deleteName}  >删除</button>
+                                                        <button className="fn-btn fn-btn-primary" data-index = {i} onClick = {Controller.Actions.deleteName}  >删除</button>
                                                     </td>
                                                 </tr>
 
@@ -101,7 +66,7 @@ define(function(require,exports,module){
 
                             <div className = "fn-MT20 fn-W300 fn-LH30 fn-MT20 ">
                                 <input  ref="myInput" type="text" className="fn-input-text" placeholder="请输入姓名" maxLength="20"/>
-                                <button className="fn-btn fn-btn-default fn-LH28"  >增加</button>
+                                <button className="fn-btn fn-btn-default fn-LH28" onClick = {self.createName} >增加</button>
                             </div>
                     </div>
                 )
