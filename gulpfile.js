053a466d9bf6c22c9d2aaff6782d7073090d838c
apple <apple@appledeMacBook-Pro.local>
2016-07-25 07:52:01 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,55 @@
 // 定义
 const gulp = require('gulp'),
-    babel = require('gulp-babel');
+      babel = require('gulp-babel'),
+      less = require('gulp-less'),
+      cssmin = require('gulp-clean-css'),
+      sourceMap = require('gulp-sourcemaps'),
+      browserSync = require('browser-sync'),
+      util = require('gulp-util'),
+      notify = require('gulp-notify'),
+      plumber = require('gulp-plumber');
     
  
 // 任务
-gulp.task('default', ['watch']);
+gulp.task('default', ['brow','watch']);
 
-//观察者
-// var matchRex = /(src.*)\/.*\..*/;
-// gulp.task('watch', () => {
-//     gulp.watch(['src/**/*.babel', 'src/**/*.jsx'])
-//         .on('change', (e) => {
-//             var match = e.path.replace(/\\/g, '/').match( matchRex ),
-//                 file = match[0];
-//                 console.log(e)
-//             gulp.src( file )
-//                 .pipe( babel( { presets: ['es2015', 'react'] } ).on('error', (e) => {
-//                     console.error( "error", e.message);
-//                 }) )
-//                 .pipe(gulp.dest( match[1] ));
-             
-//         });
-// });
-//babel
- // gulp.task('babel',() => {
- //     gulp.src('src/**/*.jsx')
- //     .pipe(babel({presets:['es2015','react']}).on("error",function(e){
- //            console.error("error",e.message);
- //     }))
- //     .pipe(gulp.dest('src'))
-
- // });
- // gulp.task('watch',() => {
- //   gulp.watch(['src/**/*.babel','src/**/*.jsx'],['babel'])
- //   .on('change',(e) => {
- //    console.log(e.path+" was "+e.type+',running tasks....[babel]')
- //   })
-
- // })
- // //观察者
- var matchRex = /(src.*)\/.*\.*/;
+  //观察者
+let matchRex = /(src.*)\/.*\.*/ ;
+ let babelTask = (e) => {
+   let match = e.path.replace(/\\/g,"/").match(matchRex),
+       files = match[0],
+       paths = match[1];
+       gulp.src(files)
+       .pipe(babel({presets:['es2015','react','stage-0']}).on("error",(e) =>{
+         console.error("error"+e.message);
+       })).pipe(gulp.dest(paths));
+       
+ }
    gulp.task("watch",() =>{
      gulp.watch(['src/**/*.babel','src/**/*.jsx']).on('change',(e)=>{
-         var match = e.path.replace(/\\/g,"/").match(matchRex),
-            files = match[0];
-            gulp.src(files)
-            .pipe(babel({presets:['es2015','react','stage-0']}).on('error',(e) =>{
-              console.error('error:'+e.message);
-            }))
-            .pipe(gulp.dest(match[1]));
+         console.log('files:'+e.path+'was'+e.type+',running tasks ....[babel]');
+         babelTask(e);
      })
+     gulp.watch('./src/css/**/*.less',['less'])
+   }) 
 
-   })  
+//less
+gulp.task('less',()=>{
+  return gulp.src('./src/css/**/*.less')
+         .pipe(less())
+         .on('error',(error)=>{
+          util.log('less error:'+error.message);
+         })
+         .pipe(gulp.dest('./src/css'))
+        
+})
 
-// // 静态服务
-// gulp.task('brow', () => {
-//     browserSync({
-//         files: ['src/css/**/*.css', 'src/js/**/*.js', 'src/html/**/*.html'],
-//         server: {
-//             baseDir: "./"
-//         }
-//     });
-// });
+// 静态服务
+gulp.task('brow', () => {
+    browserSync({
+        files: ['./src/css/**/*.css', './src/js/**/*.js', './src/html/**/*.html'],
+        server: {
+            baseDir: "./"
+        }
+    });
+});
