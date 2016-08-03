6ef0a8f4476d71b9b19f77291e42aa15a8f4c20e
wuxikai <[user>
2016-08-03 09:28:43 +0800

    add

--- src/js/bus/postMssage/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-define(function(require,exports,module){
-	$(function(){
-		$("iframe1")[0].contentWindow.postMessage("welcome to my home!~~","http://wu-xiao-wen.github.io")
-
-	})
-})
