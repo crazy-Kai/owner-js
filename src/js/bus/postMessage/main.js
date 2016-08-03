da076fd9e1fa31dd8ba5413247ffd7707c6d0f2c
wuxikai <[user>
2016-08-03 10:41:05 +0800

    add

--- src/js/bus/postMessage/main.js
+++ src/js/bus/postMessage/main.js
@@ -1,13 +1,17 @@
 "use strict";
 define(function(require,exports,module){
-	window.onload = function(){
+	window.onload= function(){
 		var fram = document.getElementById("iframe1");
-		fram.contentWindow.postMessage("welcome to my home!~~","http://127.0.0.1:8004")
+		fram.contentWindow.postMessage("welcome to my home!~~","http://127.0.0.1:8007")
 
 	}
 
 	window.addEventListener("message",function(event){
+		if(event.origin === "http://127.0.0.1:8007"){
+
+		alert("接收子窗口的数据")
 	      $("#message").html(event.data)
+		}
 
 	},false)
 	
