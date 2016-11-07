44850d422a6fcafe30018e6dba9603ab7c66c83c
wuxikai <[user>
2016-11-07 12:27:34 +0800

    add

--- dist/myReact/jsx/dataRow-debug.js
+++ dist/myReact/jsx/dataRow-debug.js
@@ -129,3 +129,71 @@ define("js/bus/myReact/jsx/pushbutton-debug", ["common/react-debug", "bus/myReac
         });
     module.exports = Pushbutton
 });
+"use strict";
+define("js/bus/myReact/jsx/pushbutton-debug", ["common/react-debug", "bus/myReact/controller/connectActions-debug", "bus/myReact/controller/listenToActions-debug"], function(require, exports, module) {
+    var React = require("common/react-debug"),
+        ConnectActions = require("bus/myReact/controller/connectActions-debug"),
+        ListenToActions = require("bus/myReact/controller/listenToActions-debug"),
+        Pushbutton = React.createClass({
+            displayName: "Pushbutton",
+            getInitialState: function() {
+                return {}
+            },
+            clickButton: function(e) {
+                switch (e.preventDefault(), e.stopPropagation(), this.props.btnName) {
+                    case "添加":
+                        ConnectActions.add();
+                        break;
+                    case "修改":
+                        this.props.callbackParent(this.props.data);
+                        break;
+                    case "删除":
+                        ListenToActions["delete"](this.props.data);
+                        break;
+                    default:
+                        this.props.callbackParent()
+                }
+            },
+            render: function() {
+                return React.createElement("button", {
+                    className: this.props.className,
+                    onClick: this.clickButton
+                }, this.props.btnName)
+            }
+        });
+    module.exports = Pushbutton
+});
+"use strict";
+define("js/bus/myReact/jsx/pushbutton-debug", ["common/react-debug", "bus/myReact/controller/connectActions-debug", "bus/myReact/controller/listenToActions-debug"], function(require, exports, module) {
+    var React = require("common/react-debug"),
+        ConnectActions = require("bus/myReact/controller/connectActions-debug"),
+        ListenToActions = require("bus/myReact/controller/listenToActions-debug"),
+        Pushbutton = React.createClass({
+            displayName: "Pushbutton",
+            getInitialState: function() {
+                return {}
+            },
+            clickButton: function(e) {
+                switch (e.preventDefault(), e.stopPropagation(), this.props.btnName) {
+                    case "添加":
+                        ConnectActions.add();
+                        break;
+                    case "修改":
+                        this.props.callbackParent(this.props.data);
+                        break;
+                    case "删除":
+                        ListenToActions["delete"](this.props.data);
+                        break;
+                    default:
+                        this.props.callbackParent()
+                }
+            },
+            render: function() {
+                return React.createElement("button", {
+                    className: this.props.className,
+                    onClick: this.clickButton
+                }, this.props.btnName)
+            }
+        });
+    module.exports = Pushbutton
+});
--- dist/react/reflux/main-debug.js
+++ dist/react/reflux/main-debug.js
@@ -259,3 +259,103 @@ define("js/bus/react/reflux/controller-debug", ["common/reflux-debug"], function
         Reflux: Reflux
     }
 });
+define("js/bus/react/reflux/controller-debug", ["common/reflux-debug"], function(require, exports, module) {
+    var Reflux = require("common/reflux-debug"),
+        TableActions = Reflux.createActions(["deleteName", "addName", "editName"]),
+        TableStore = Reflux.createStore({
+            listenables: [TableActions],
+            getInitialState: function() {
+                return this.store
+            },
+            store: {
+                data: [{
+                    name: "wuxiaowen"
+                }, {
+                    name: "wukai"
+                }, {
+                    name: "一丙"
+                }, {
+                    name: "保健"
+                }],
+                key: ""
+            },
+            onDeleteName: function(event) {
+                var self = this,
+                    target = $(event.target),
+                    index = target.data("index");
+                self.store.data.splice(index - 1, 1), self.trigger(self.store)
+            },
+            onEditName: function(event, myInput) {
+                var self = this,
+                    index = $(event.target).data("index"),
+                    key = index - 1,
+                    value = self.store.data[key].name,
+                    input = myInput;
+                input.value = value, self.store.key = key, input.nextSibling.textContent = "保存", input.focus(), self.trigger(self.store)
+            },
+            onAddName: function(event, myInput) {
+                var self = this,
+                    input = myInput,
+                    keys = self.store.key,
+                    targetName = $(event.target).text();
+                "保存" === targetName ? (self.store.data[keys].name = input.value, input.value = "", $(event.target).text("增加")) : "增加" === targetName && (self.store.data.push({
+                    name: input.value
+                }), input = "", self.trigger(self.store))
+            }
+        });
+    module.exports = {
+        Actions: TableActions,
+        Store: TableStore,
+        Reflux: Reflux
+    }
+});
+define("js/bus/react/reflux/controller-debug", ["common/reflux-debug"], function(require, exports, module) {
+    var Reflux = require("common/reflux-debug"),
+        TableActions = Reflux.createActions(["deleteName", "addName", "editName"]),
+        TableStore = Reflux.createStore({
+            listenables: [TableActions],
+            getInitialState: function() {
+                return this.store
+            },
+            store: {
+                data: [{
+                    name: "wuxiaowen"
+                }, {
+                    name: "wukai"
+                }, {
+                    name: "一丙"
+                }, {
+                    name: "保健"
+                }],
+                key: ""
+            },
+            onDeleteName: function(event) {
+                var self = this,
+                    target = $(event.target),
+                    index = target.data("index");
+                self.store.data.splice(index - 1, 1), self.trigger(self.store)
+            },
+            onEditName: function(event, myInput) {
+                var self = this,
+                    index = $(event.target).data("index"),
+                    key = index - 1,
+                    value = self.store.data[key].name,
+                    input = myInput;
+                input.value = value, self.store.key = key, input.nextSibling.textContent = "保存", input.focus(), self.trigger(self.store)
+            },
+            onAddName: function(event, myInput) {
+                var self = this,
+                    input = myInput,
+                    keys = self.store.key,
+                    targetName = $(event.target).text();
+                "保存" === targetName ? (self.store.data[keys].name = input.value, input.value = "", $(event.target).text("增加")) : "增加" === targetName && (self.store.data.push({
+                    name: input.value
+                }), input = "", self.trigger(self.store))
+            }
+        });
+    module.exports = {
+        Actions: TableActions,
+        Store: TableStore,
+        Reflux: Reflux
+    }
+});
